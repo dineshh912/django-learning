0fddad40cb4de4117851360f40019847626737c3
dineshh912 <dineshh912@gmail.com>
2022-04-19 09:30:21 +0100

    rest_lib

---
+++ 08_library_rest_api/api/views.py
@@ -0,0 +1,8 @@
+from rest_framework.generics import ListAPIView
+from books.models import Book
+from .serializers import BookSerializer
+# Create your views here.
+
+class BookApiView(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer

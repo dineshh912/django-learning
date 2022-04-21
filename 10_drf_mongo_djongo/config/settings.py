93c59aa69ccc540f90cf9faf9129c4969297592e
dineshh912 <dineshh912@gmail.com>
2022-04-21 12:04:53 +0100

    remove

--- 10_drf_mongo_djongo/config/settings.py
+++ 10_drf_mongo_djongo/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gemat=805+!p+%i6@g_d(zzwzv6gqo4^hf!^#^(on4mbfffy%f'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

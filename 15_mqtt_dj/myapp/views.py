68decf9a9093afc18ff70f614a131ea3c67750d7
dineshh912 <dineshh912@gmail.com>
2022-08-15 11:45:52 +0100

    mqtt

--- 15_mqtt_dj/myapp/views.py
+++ 15_mqtt_dj/myapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
+import djangoiot
 
 # Create your views here.
+class HomeView(View):
+
+    def get(self, request, *args, **kwargs):
+        new_broker = djangoiot.broker.register(host='localhost', port=1883)
+        return HttpResponse("Hello World")
+
+    
